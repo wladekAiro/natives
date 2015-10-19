e2e8c974f80e30a2fe305badd8e4211adb6e0199
wladek.airo@gmail.com <wladek.airo@gmail.com>
2015-10-19 20:12:00 +0300

    small changes on title

--- resources/views/layout/resturantlayout.blade.php
+++ resources/views/layout/resturantlayout.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>C&R</title>
+  <title>Natives</title>
 
   <link href="/css/app.css" rel="stylesheet">
   <link href="/css/club.css" rel="stylesheet">
